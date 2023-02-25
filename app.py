35093cb72f3b8eaa246b63deef79ecba449e9721
Sandy <sandeepnambiar02.com>
2023-02-25 11:49:57 +0530

    upload function

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-import flask
+from flask import Flask, render_template, request, redirect, url_for
 
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
